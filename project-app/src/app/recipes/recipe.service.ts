ae10593b576927628f5b2c34f2b3d6d8edec742a
FriptuT <friptut@gmail.com>
2021-05-01 21:36:26 +0300

    adding routing to the project

--- project-app/src/app/recipes/recipe.service.ts
+++ project-app/src/app/recipes/recipe.service.ts
@@ -12,14 +12,14 @@ export class RecipeService {
    private recipes: Recipe[] = [
         new Recipe('Tasty Schnitzel', 
         'A super-tasty Schnitzel - just awesome!', 
-        'https://www.recipetineats.com/wp-content/uploads/2020/02/Honey-Garlic-Chicken-Breast_5-SQ.jpg',
+        'https://previews.123rf.com/images/foodandmore/foodandmore1502/foodandmore150200151/36578074-close-up-gourmet-tasty-crumbled-schnitzel-and-crispy-fries-with-lemon-tomato-and-lettuce-.jpg',
         [
             new Ingredient("Meat", 1),
             new Ingredient("French Fries", 20)
         ]),
         new Recipe('Big Fat Burger', 
         'What else you need to say?', 
-        'https://www.recipetineats.com/wp-content/uploads/2020/02/Honey-Garlic-Chicken-Breast_5-SQ.jpg',
+        'https://1.bp.blogspot.com/-JhgDc-MCuJI/X3Mmx8xlxZI/AAAAAAAA-sw/8aBUdMfXHf4mw1mVK1NopftesDteb-LDQCLcBGAsYHQ/s960/Fatburger.jpg',
         [
             new Ingredient("Buns", 2),
             new Ingredient("Meat", 1)
@@ -33,6 +33,10 @@ export class RecipeService {
 
       getRecipes() {
           return this.recipes.slice();
+      }
+
+      getRecipe(index: number){
+          return this.recipes[index];
       }
 
       addIngredientsToShoppingList(ingredients: Ingredient[]){
